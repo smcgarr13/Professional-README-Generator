6e388b9374fdd5927a9687aa879847085a99ff93
Sparkles <smcgarr13@yahoo.com>
2023-02-09 22:12:18 -0600

    generate-markdown-functions

--- assets/js/generateMarkdown.js
+++ assets/js/generateMarkdown.js
@@ -14,16 +14,33 @@ function renderLicenseLink(license) {
   if (!license) {
     return '';
   }
+
+  switch (license) {
+    case 'MIT':
+      return 'https://opensource.org/licenses/MIT';
+    case 'Apache 2.0':
+      return 'https://www.apache.org/licenses/LICENSE-2.0';
+    case 'ISC':
+      return 'https://opensource.org/licenses/ISC';
+      case 'none':
+      return '';
+  }
 }
 
 // TODO: Create a function that returns the license section of README
 // If there is no license, return an empty string
-function renderLicenseSection(license) {}
+function renderLicenseSection(license) {
+  if (license != 'none') {
+    return `## license
+    ${license}`;
+  }
+}
 
 // TODO: Create a function to generate markdown for README
 function generateMarkdown(data) {
   return `# ${data.title}
-
+  ${renderLicenseBadge(data.license)}
+  ${renderLicenseSection(data.license)}
 `;
 }
 
